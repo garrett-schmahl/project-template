c87bb35945710a07920eddf5a477979b92953484
Garrett Schmahl <gschmahl@pm.me>
2021-03-25 07:07:52 -0700

    initialize repository

---
+++ gitInit.bat
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "initialize repository"
+git branch -M main
+git remome add origin https://github.com/garrett-schmahl/project-template.git
+git push -u origin main
